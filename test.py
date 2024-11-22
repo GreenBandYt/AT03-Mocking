7cd99b32455ec2213a07681e444be94bf6532c39
Yuriy Bandura <bandurayv@gmail.com>
2024-11-22 22:12:20 +0300

    Add_Part3

--- test.py
+++ test.py
@@ -1,45 +1,39 @@
 import pytest
-from main import get_weather
+from main import get_github_user
 
-def test_get_weather(mocker):
+def test_get_github_user(mocker):
     """
-    Тестируем функцию get_weather с использованием мока для requests.get.
+    Тестируем функцию get_github_user с использованием мока для requests.get.
     """
     # Мокаем requests.get
     mock_get = mocker.patch('main.requests.get')
     mock_get.return_value.status_code = 200
     mock_get.return_value.json.return_value = {
-        'weather': [{'description': 'sunny'}],
-        'main': {'temp': 25}
+        'login': 'johndoe',
+        'name': 'John Doe',
+        'public_repos': 5
     }
 
-    # Вызов функции с мокированным requests.get
-    api_key = 'test_api_key'
-    city = 'New York'
-    weather_data = get_weather(api_key, city)
+    # Вызываем функцию get_github_user
+    result = get_github_user('johndoe')
 
-    # Проверка результата
-    assert weather_data == {
-        'weather': [{'description': 'sunny'}],
-        'main': {'temp': 25}
+    # Проверяем, что результат соответствует ожидаемому
+    assert result == {
+        'login': 'johndoe',
+        'name': 'John Doe',
+        'public_repos': 5
     }
 
-    # Проверяем, что requests.get был вызван с правильным URL
-    mock_get.assert_called_once_with(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}")
-
-
-def test_get_weather_error(mocker):
+def test_get_github_user_with_error(mocker):
     """
-    Тестируем функцию get_weather с использованием мока для requests.get.
+    Тестируем функцию get_github_user с использованием мока для requests.get, возвращающего ошибку.
     """
     # Мокаем requests.get
     mock_get = mocker.patch('main.requests.get')
-    mock_get.return_value.status_code = 404
+    mock_get.return_value.status_code = 500
 
-    # Вызов функции с мокированным requests.get
-    api_key = 'test_api_key'
-    city = 'Nonexistent City'
-    weather_data = get_weather(api_key, city)
+    # Вызываем функцию get_github_user
+    result = get_github_user('cat')
 
-    # Проверка результата
-    assert weather_data is None
+    # Проверяем, что результат соответствует ожидаемому
+    assert result is None
